4a5ee6b99f78f3097c87638a7266717a5aba58e1
vgupta9207 <91655979+vgupta9207@users.noreply.github.com>
2022-03-20 23:38:08 -0700

    Feat: Edited authentication with JWT token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,31 @@
-const User = require("../models/User.js");
+const User = require('../models/User.js');
 
-const loginuser = (req, res) => {
-  User.findOne({
-    name: req.body.name,
-  })
-
-    .then((user) => {
-      console.log(req.url);
-      if (!user) {
-        return res.status(401).json({
-          error: new Error("User not found!"),
-        });
-      }
-      if (req.body.password == user.password) {
-        return res.status(200).json({
-          data: user,
-          url: `/signin/${user._id}`,
-          message: "User was authenticataed successfully",
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error });
-    });
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    const token = user.generateJWT();
+    res.status(201).json({ user, token: token });
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating user.' });
+    console.log(error);
+  }
 };
 
-module.exports = loginuser;
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new Error('Please enter email or password');
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Invalid email or password');
+  }
+  const correctPassword = await user.checkPassword(password);
+  if (!correctPassword) {
+    throw new Error('Invalid email or password');
+  }
+  const token = user.generateJWT();
+  res.status(200).json({ user, token });
+};
+
+module.exports = { createUser, loginUser };
