4c349fd5cd72be9e30b1c93ccc91794495382d5e
vgupta9207 <91655979+vgupta9207@users.noreply.github.com>
2022-03-30 00:06:54 -0700

    Fix: Updated the login user function code

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,19 +15,19 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
     res.status(500).json({ message: 'Please enter email or password' });
-
   }
-  const user = await User.findOne({ email });
-  if (!user) {
+  try {
+    const user = await User.findOne({ email });
+    const correctPassword = await user.checkPassword(password);
+    if (!user || !correctPassword) {
+      res.status(500).json({ message: 'Invalid email or password' });
+    } else {
+      const token = user.generateJWT();
+      res.status(200).json({ user, token });
+    }
+  } catch (err) {
     res.status(500).json({ message: 'Invalid email or password' });
-   
   }
-  const correctPassword = await user.checkPassword(password);
-  if (!correctPassword) {
-    res.status(500).json({ message: 'Invalid email or password' });
-  }
-  const token = user.generateJWT();
-  res.status(200).json({ user, token });
 };
 
 module.exports = { createUser, loginUser };
